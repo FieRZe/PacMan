65c296f7748a0795e3f7cf13c67ade6653c40bea
FieRZe <60667841+FieRZe@users.noreply.github.com>
2020-09-13 14:43:19 +0300

    Added Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	Rigidbody2D rb2d;
+
+	public float speed; // the speedpacman can travel
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    	// moving left
+        if (Input.GetAxis("Horizontal") < 0 ) 
+        {
+            
+            rb2d.velocity = Vector2.left * speed;
+        }
+        // moving right
+        if (Input.GetAxis("Horizontal") > 0 )
+        {
+            
+            rb2d.velocity = Vector2.right * speed;
+        }
+        // moving down
+        if (Input.GetAxis("Vertical") < 0 )
+        {
+            
+            rb2d.velocity = Vector2.down * speed;
+        }
+        // moving up
+        if (Input.GetAxis("Vertical") > 0 )
+        {
+            
+            rb2d.velocity = Vector2.up * speed;
+        }
+        
+        
+    }
+}
