c644691c54688dd6ec8d36d3a2346a0bc833a36a
FieRZe <60667841+FieRZe@users.noreply.github.com>
2020-09-19 21:21:02 +0300

    Adjusted Player Control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 
 	public float speed; // the speedpacman can travel
     // Start is called before the first frame update
+
+	private Vector2 direction; // the direction pacman is going
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -19,30 +22,45 @@ public class PlayerController : MonoBehaviour
     	// moving left
         if (Input.GetAxis("Horizontal") < 0 ) 
         {
-            rb2d.velocity = Vector2.left * speed;
-            transform.up = Vector2.left;
+            direction = Vector2.left;
         }
         // moving right
         if (Input.GetAxis("Horizontal") > 0 )
         {
-            rb2d.velocity = Vector2.right * speed;
-            transform.up = Vector2.right;
+            direction = Vector2.right;
         }
         // moving down
         if (Input.GetAxis("Vertical") < 0 )
         {
             
-            rb2d.velocity = Vector2.down * speed;
-            transform.up = Vector2.down;
+            direction = Vector2.down;
         }
         // moving up
         if (Input.GetAxis("Vertical") > 0 )
         {
             
-            rb2d.velocity = Vector2.up * speed;
-            transform.up = Vector2.up;
+            direction = Vector2.up;
         }
         
+        // To avoid decreasing speed when you touch boxcolliders we need to set our speed to constant
+        // no matter what
+
+        rb2d.velocity = direction * speed; // but in this case we need to remember what direction is going
+		transform.up = direction; 
+
+
+		if (rb2d.velocity.x == 0) // meaning that he's going up and down by 'y'
+		{
+			// that won't work in some games but in this game it'll work
+			transform.position = new Vector2(Mathf.Round(transform.position.x), transform.position.y );
+
+		}
         
+        if (rb2d.velocity.y == 0) // meaning that he's going up and down by 'y'
+		{
+			// that won't work in some games but in this game it'll work
+			transform.position = new Vector2(transform.position.x, Mathf.Round(transform.position.y));
+
+		}
     }
 }
